fd0956917e60a13c491d0d112a4a84ad901cf570
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2024-06-18 10:12:06 +0800

    user roles seeder

---
+++ database/seeders/UserRolesSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\UserRole;
+
+class UserRolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $roles = ['superadmin', 'staff', 'teacher', 'student'];
+
+        foreach ($roles as $role)
+        {
+            UserRole::create([
+                'name'          => $role,
+                'is_enabled'    => true
+            ]);
+        }
+    }
+}
