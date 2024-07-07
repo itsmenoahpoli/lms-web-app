173273cc62784b993b8c1f577436b77e6ae40d4d
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2024-07-07 23:23:25 +0800

    lectures module wip

---
+++ app/Models/Lectures/Lecture.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Lectures;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Lecture extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+}
